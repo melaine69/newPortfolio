e1bd3ba9a835a4916448de539f526b937fa3b77c
Mélaine <113093304+melaine69@users.noreply.github.com>
2024-02-10 11:04:56 +0100

    Intégration contenu  header et footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,15 +1,57 @@
+import clsx from "clsx"
+import ArrowRight from "./icons/arrowRight"
+import Email from "./icons/email"
+import Github from "./icons/github"
+import Linkedin from "./icons/linkedin"
 export default function Footer() {
 
+    const links = [
+        {
+            href: "https://www.linkedin.com/in/melaine-dewitte/",
+            label: "Linkedin",
+            icon: Linkedin,
+        },
+        {
+            href: "https://github.com/melaine69",
+            label: "Github",
+            icon: Github,
+        },
+        {
+            href: "mailto:melaine.dewitte@icloud.com",
+            label: "Email",
+            icon: Email,
+        },
+
+    ]
+
     function getDayName(date = new Date(), locale = "fr-FR") {
-        return date.toLocaleDateString(locale, {weekday: "long"})
+        return date.toLocaleDateString(locale, { weekday: "long" })
     }
     return (
         <footer className="flex-shrink-0">
-            <div className="mx-auto p-6 max-w-6xl text-lg">
+            <div className="mx-auto px-6 p-20 max-w-6xl space-y-20 text-lg">
+                <div className="grid sm:grid-cols-3 gap-12 mx-auto max-w-4xl">
+                    {links.map((link) => (
+                        <a
+                            className={clsx("group flex items-center gap-2 text-gray-800 py-3 border-b transition transform hover:-translate-y-1",
+                                {
+                                    "hover:text-[#0D74CE]": link.label === "Linkedin",
+                                    "hover:text-black": link.label === "Github",
+                                    "hover:text-primary": link.label === "Email",
+                                })}
+                            href={link.href}
+                            key={link.href}
+                        >
+                            <link.icon className="w-6" />
+                            <span>{link.label}</span>
+                            <ArrowRight className="ml-auto w-6 text-gray-800 transition tranform group-hover:-rotate-[45deg]" />
+                        </a>
+                    ))}
+                </div>
                 <p className="text-gray-500 text-center">
-                © {new Date().getFullYear()} Mélaine Dewitte - Bon {getDayName()} !
+                    © {new Date().getFullYear()} Mélaine Dewitte - Bon {getDayName()} !
                 </p>
             </div>
-        </footer>
+        </footer >
     )
 }
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,18 +5,18 @@ export default function Header() {
     return (
         <header>
 
-            <div className="flex items-center mx-auto p-6 max-w-6xl text-lg">
-                <div className="flex-1 flex justify-start">
+            <div className="relative flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-0 mx-auto p-6 max-w-6xl text-lg">
+                <div className="sm:flex-1 flex justify-start">
                     <Link title="Mélaine.dev" className="font-bold" href="/">Mélaine.dev</Link>
                 </div>
-                <nav className="flex-1 flex justify-center">
-                    <ul className="flex-shrink-0 flex items-center gap-6">
+                <nav className="sm:flex-1 flex sm:justify-center">
+                    <ul className="flex-shrink-0 flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-6">
                         <li><Link href="/">Accueil</Link></li>
                         <li><Link href="/">À propos</Link></li>
                         <li><Link href="/">Réalisations</Link></li>
                     </ul>
                 </nav>
-                <div className="flex-1 flex justify-end">
+                <div className="absolute top-6 right-6 sm:relative sm:top-0 sm:right-6 sm:flex-1 sm:flex sm:justify-end">
                     <Button>Contact</Button>
                 </div>
             </div>
