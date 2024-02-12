df85e87ecc5994a0eb96f9e1aa20b7fbd112f6e7
Mélaine <113093304+melaine69@users.noreply.github.com>
2024-02-12 09:54:06 +0100

    Correction erreur synthaxe pour déploiement

--- app/about.tsx
+++ app/about.tsx
@@ -38,7 +38,7 @@ export default function About() {
             viewport={{ once: true }}
             className="text-3xl md:text-4xl leading-snug md:leading-snug wavy"
           >
-              Je suis développeuse full-stack. J'aime l'aspect <span className="font-semibold text-primary">créatif</span> du métier, le fait de <span className="text-primary font-semibold italic">construire</span> un projet brique par brique et collaborer avec une équipe pluridisciplinaire. Je suis actuellement en formation chez la Wild Code School et je recherche une <span className="bg-primary rounded-xl px-3 text-white font-semibold">alternance</span> pour cette année ! {" "}
+              Je suis développeuse full-stack. J&#39;aime l'aspect <span className="font-semibold text-primary">créatif</span> du métier, le fait de <span className="text-primary font-semibold italic">construire</span> un projet brique par brique et collaborer avec une équipe pluridisciplinaire. Je suis actuellement en formation chez la Wild Code School et je recherche une <span className="bg-primary rounded-xl px-3 text-white font-semibold">alternance</span> pour cette année ! {" "}
             </motion.p>
 
             <motion.p className="text-2xl leading-relaxed text-gray-600"
@@ -49,7 +49,7 @@ export default function About() {
             }}
             viewport={{ once: true }}
           >
-              Je m'épanouis pleinement dans les environnements <span className="text-orange-400 underline">collaboratifs</span>. Ma flexibilité me permet d'alterner entre des tâches individuelles, où je peux exprimer ma créativité, et des projets collectifs, où la synergie des idées s'épanouit.
+              Je m&#39;épanouis pleinement dans les environnements <span className="text-orange-400 underline">collaboratifs</span>. Ma flexibilité me permet d&#39;alterner entre des tâches individuelles, où je peux exprimer ma créativité, et des projets collectifs, où la synergie des idées s&#39;épanouit.
             </motion.p>
           </div>
         </div>
